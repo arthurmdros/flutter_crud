4659c8ad143951dad0c3e005ed50033825837a48
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-15 12:04:03 -0300

    Criando provider para usuários

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:crud/provider/users_provider.dart';
 import 'package:crud/views/user_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter-CRUD',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: UserList(),
+    return ChangeNotifierProvider(
+      create: (ctx) => Users(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter-CRUD',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: UserList(),
+      )
     );
   }
 }
