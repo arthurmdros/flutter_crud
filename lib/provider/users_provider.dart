463bf80807eae43c21ad888231ef39fa8c5b68e0
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-15 13:56:02 -0300

    Adicionar e atulizar um usuário

--- lib/provider/users_provider.dart
+++ lib/provider/users_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:crud/data/dummy_users.dart';
 import 'package:crud/models/user.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,5 +17,28 @@ class Users with ChangeNotifier{
 
   User byIndex(int i){
     return _items.values.elementAt(i);
+  }
+
+  void submit (User user){
+    if(user == null) return;
+
+    if( user.id != null && user.id.toString().trim().isNotEmpty && _items.containsKey(user.id)){
+      _items.update(user.id.toString(), (_) => User(
+          id: user.id,
+          name: user.name, 
+          email: user.email,
+          avatarUrl: user.avatarUrl
+        )
+      );
+    }else{
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(id, () => User(
+        id: id,
+        name: user.name,
+        email: user.email,
+        avatarUrl: user.avatarUrl
+      )); 
+    }
+    notifyListeners();
   }
 }
