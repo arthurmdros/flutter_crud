fd99ffc246360fec99403a96311df4b61e2dbfe2
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-15 16:44:48 -0300

    Navegação para o formulaŕio

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,6 +1,8 @@
 import 'package:crud/data/dummy_users.dart';
+import 'package:crud/models/user.dart';
 import 'package:crud/provider/users_provider.dart';
-import 'package:crud/views/user_file.dart';
+import 'package:crud/components/user_tile.dart';
+import 'package:crud/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,7 +16,9 @@ class UserList extends StatelessWidget {
         centerTitle: true,
         title: Text('Lista de usuários'),
         actions: <Widget>[
-          IconButton(onPressed: (){}, icon: Icon(Icons.add))
+          IconButton(onPressed: (){
+            Navigator.of(context).pushNamed(AppRoutes.USER_FORM);
+          }, icon: Icon(Icons.add))
         ],
       ),
       body: ListView.builder(
