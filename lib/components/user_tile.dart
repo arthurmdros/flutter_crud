7897d1f76ba093dbb055360348f966a47525dd62
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-15 18:22:01 -0300

    Metódo de excluir um usuário

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,6 +1,8 @@
 import 'package:crud/models/user.dart';
+import 'package:crud/provider/users_provider.dart';
 import 'package:crud/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   
@@ -24,7 +26,28 @@ class UserTile extends StatelessWidget {
             IconButton(onPressed: (){
               Navigator.of(context).pushNamed(AppRoutes.USER_FORM, arguments: user);
             }, color: Colors.indigoAccent, icon: Icon(Icons.edit)),
-            IconButton(onPressed: (){}, color: Colors.redAccent, icon: Icon(Icons.delete))
+            IconButton(onPressed: (){
+              showDialog(
+                context: context, 
+                builder: (ctx) => AlertDialog(
+                  title: Text('Excluir usuário'),
+                  content: Text('Deseja excluir o usuário: ' + user.name + ' ?'),
+                  actions: [
+                    TextButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(context).pop(true),
+                      ),
+                  ],
+                )).then((confirmed) {
+                  if (confirmed) {
+                    Provider.of<Users>(context, listen: false).remove(user);
+                  }
+                });
+            }, color: Colors.redAccent, icon: Icon(Icons.delete))
           ],
         ),
       ),
