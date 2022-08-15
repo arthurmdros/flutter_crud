7e99aedb51ac6bd114617fe73dd3c7af9a1d0419
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-15 11:44:29 -0300

    Removendo obrigatoriedade de uma URL pro avatar

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,12 +4,12 @@ class User {
   final String? id;
   final String name;
   final String email;
-  final String avatarUrl;
+  final String? avatarUrl;
 
   const User({
     this.id,
     required this.name,
     required this.email,
-    required this.avatarUrl,
+    this.avatarUrl,
   });
 }
